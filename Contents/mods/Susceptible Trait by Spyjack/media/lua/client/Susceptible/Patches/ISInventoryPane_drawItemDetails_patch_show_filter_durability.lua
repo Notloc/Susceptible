3feab211ef3a6d524a948a6566ebc0f69de4de05
Notloc <ColtonJamesCampbell@gmail.com>
2022-05-17 19:58:17 -0300

    Added filter bar when viewing mask item details in inventory.

---
+++ Contents/mods/Susceptible Trait by Spyjack/media/lua/client/Susceptible/Patches/ISInventoryPane_drawItemDetails_patch_show_filter_durability.lua
@@ -0,0 +1,37 @@
+require "Susceptible/SusceptibleMaskData"
+
+local fgBar = {r=0.0, g=0.6, b=0.0, a=0.7}
+local fgText = {r=0.6, g=0.8, b=0.5, a=0.6}
+
+ISInventoryPane.drawItemDetails_prepatch_susceptible = ISInventoryPane.drawItemDetails;
+
+ISInventoryPane.drawItemDetails = function(self, item, y, xoff, yoff, red)
+	if item == nil then
+        return;
+    end
+
+	self:drawItemDetails_prepatch_susceptible(item, y, xoff, yoff, red);
+
+	local top = self.headerHgt + y * self.itemHgt + yoff
+
+	if SusceptibleMaskItems[item:getType()] then
+		local data = item:getModData()
+		local delta = 1;
+		if data.filterDurability then
+			delta = data.filterDurability / data.filterDurabilityMax;
+		end
+
+		self:drawSuceptibleConditionProgressBar(item:getName(), delta, xoff, top, fgText, fgBar)
+	end
+end
+
+function ISInventoryPane:drawSuceptibleConditionProgressBar(itemName, fraction, xoff, top, fgText, fgBar)
+	local textWid = getTextManager():MeasureStringX(self.font, itemName)
+
+	local filterText = getText("UI_Susceptible_Filter");
+	local xoff2 = 40 + math.max(120, 50 + textWid) + xoff;
+	self:drawText(filterText, xoff2, top + (self.itemHgt - self.fontHgt) / 2, fgText.a, fgText.r, fgText.g, fgText.b, self.font)
+
+	textWid = getTextManager():MeasureStringX(self.font, filterText)
+	self:drawProgressBar(15 + textWid + xoff2 + xoff, top+(self.itemHgt/2)-1, 100, 2, fraction, fgBar)
+end
