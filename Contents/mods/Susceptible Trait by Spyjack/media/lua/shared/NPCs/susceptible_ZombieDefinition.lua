953e9a017c10ebd46d7db7850a9d8f935dfe75f9
MrSunshine21 <105752255+MrSunshine21@users.noreply.github.com>
2022-05-24 20:37:23 +0100

    Changes to "special" zombies
    -Changes to clothing.xml.
    -Changes to OnDeathDistribution
    -Changes to ZombieDefinition

--- Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
+++ Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
@@ -1,8 +1,8 @@
 require 'NPCs/ZombiesZoneDefinition'
 
-local Susceptible_spawnchance_hazmat = SandboxVars.Susceptible.SpawnChance * 0.003;
-local Susceptible_spawnchance_military = SandboxVars.Susceptible.SpawnChance * 0.006;
-local Susceptible_spawnchance_military_NBC = SandboxVars.Susceptible.SpawnChance * 0.005;
+local Susceptible_spawnchance_hazmat = SandboxVars.Susceptible.SpawnChance * 0.002;
+local Susceptible_spawnchance_military = SandboxVars.Susceptible.SpawnChance * 0.005;
+local Susceptible_spawnchance_military_NBC = SandboxVars.Susceptible.SpawnChance * 0.004;
 local Susceptible_spawnchance_survivor_medicalmask_blue = SandboxVars.Susceptible.SpawnChance * 1;
 local Susceptible_spawnchance_survivor_medicalmask_green = SandboxVars.Susceptible.SpawnChance * 1;
 local Susceptible_spawnchance_survivor_dustmask = SandboxVars.Susceptible.SpawnChance * 1;
