449849da0bb9e152b52e6312d4ad7f6e2d4e2ad5
MrSunshine21 <105752255+MrSunshine21@users.noreply.github.com>
2022-05-26 23:54:24 +0100

    Changed Custom Zombie Spawns
    -Changed custom zombie outfits.
    -Reduced spawn chance for all zombie types.
    -Added police zombie types.
    -Adjusted  sandbox options.

--- Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
+++ Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
@@ -1,16 +1,20 @@
 require 'NPCs/ZombiesZoneDefinition'
 
-local Susceptible_spawnchance_hazmat = SandboxVars.Susceptible.SpawnChance * 0.2;
-local Susceptible_spawnchance_military = SandboxVars.Susceptible.SpawnChance * 0.5;
-local Susceptible_spawnchance_military_NBC = SandboxVars.Susceptible.SpawnChance * 0.4;
-local Susceptible_spawnchance_survivor_medicalmask_blue = SandboxVars.Susceptible.SpawnChance * 0.8;
-local Susceptible_spawnchance_survivor_medicalmask_green = SandboxVars.Susceptible.SpawnChance * 0.8;
-local Susceptible_spawnchance_survivor_dustmask = SandboxVars.Susceptible.SpawnChance * 1;
-local Susceptible_spawnchance_survivor_clothmask = SandboxVars.Susceptible.SpawnChance * 1;
+local Susceptible_spawnchance_hazmat = SandboxVars.Susceptible.SpawnChance * 0.01;
+local Susceptible_spawnchance_military = SandboxVars.Susceptible.SpawnChance * 0.03;
+local Susceptible_spawnchance_military_NBC = SandboxVars.Susceptible.SpawnChance * 0.02;
+local Susceptible_spawnchance_police_riot = SandboxVars.Susceptible.SpawnChance * 0.04;
+local Susceptible_spawnchance_police_gasmask = SandboxVars.Susceptible.SpawnChance * 0.04;
+local Susceptible_spawnchance_survivor_medicalmask_blue = SandboxVars.Susceptible.SpawnChance * 0.05;
+local Susceptible_spawnchance_survivor_medicalmask_green = SandboxVars.Susceptible.SpawnChance * 0.05;
+local Susceptible_spawnchance_survivor_dustmask = SandboxVars.Susceptible.SpawnChance * 0.05;
+local Susceptible_spawnchance_survivor_clothmask = SandboxVars.Susceptible.SpawnChance * 0.08;
 
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleHazmat", chance= Susceptible_spawnchance_hazmat});
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleMilitary", chance= Susceptible_spawnchance_military});
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleMilitaryNBC", chance= Susceptible_spawnchance_military_NBC});
+table.insert(ZombiesZoneDefinition.Default,{name = "SusceptiblePoliceRiot", chance= Susceptible_spawnchance_police_riot});
+table.insert(ZombiesZoneDefinition.Default,{name = "SusceptiblePoliceGasmask", chance= Susceptible_spawnchance_police_gasmask});
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleSurvivorMedicalBlue", chance= Susceptible_spawnchance_survivor_medicalmask_blue});
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleSurvivorMedicalGreen", chance= Susceptible_spawnchance_survivor_medicalmask_green});
 table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleSurvivorDust", chance= Susceptible_spawnchance_survivor_dustmask});
