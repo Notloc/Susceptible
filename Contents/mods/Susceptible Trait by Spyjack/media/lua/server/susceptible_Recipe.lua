021162a6d9c6ce233f8734ec72ae49d7f1cf7033
MrSunshine21 <105752255+MrSunshine21@users.noreply.github.com>
2022-05-27 12:07:19 +0100

    Update susceptible_Recipe.lua
    Adds open/close dust mask box

--- Contents/mods/Susceptible Trait by Spyjack/media/lua/server/susceptible_Recipe.lua
+++ Contents/mods/Susceptible Trait by Spyjack/media/lua/server/susceptible_Recipe.lua
@@ -5,3 +5,6 @@ end
 function OpenMaskBoxGreen(items, result, player)
 	player:getInventory():AddItems("Susceptible.EmptyMaskBox_Green",1);
 end
+function OpenDustmaskBox(items, result, player)
+	player:getInventory():AddItems("Susceptible.EmptyDustmaskBox",1);
+end
