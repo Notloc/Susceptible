abcf974ae3480754065df5641a37ec9a1e9e8ed4
Notloc <ColtonJamesCampbell@gmail.com>
2022-06-01 00:45:04 -0300

    Cleanup

--- Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
+++ Contents/mods/Susceptible Trait by Spyjack/media/lua/shared/NPCs/susceptible_ZombieDefinition.lua
@@ -1,19 +1,12 @@
 require 'NPCs/ZombiesZoneDefinition'
 
-local Susceptible_spawnchance_hazmat = SandboxVars.Susceptible.SpawnChance * 0.01;
-local Susceptible_spawnchance_military_highcommand = SandboxVars.Susceptible.SpawnChance * 0.005;
-local Susceptible_spawnchance_military_gasmask = SandboxVars.Susceptible.SpawnChance * 0.02;
-local Susceptible_spawnchance_military_NBC = SandboxVars.Susceptible.SpawnChance * 0.01;
-local Susceptible_spawnchance_police_gasmask = SandboxVars.Susceptible.SpawnChance * 0.03;
-local Susceptible_spawnchance_firefighter = SandboxVars.Susceptible.SpawnChance * 0.03;
-local Susceptible_spawnchance_survivor_basic = SandboxVars.Susceptible.SpawnChance * 0.06;
-local Susceptible_spawnchance_survivor_advanced = SandboxVars.Susceptible.SpawnChance * 0.04;
+local spawnChance = SandboxVars.Susceptible.SpawnChance;
 
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleHazmat", chance= Susceptible_spawnchance_hazmat});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleMilitaryHighcommand", chance= Susceptible_spawnchance_military_highcommand});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleMilitaryGasmask", chance= Susceptible_spawnchance_military_gasmask});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleMilitaryNBC", chance= Susceptible_spawnchance_military_NBC});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptiblePoliceGasmask", chance= Susceptible_spawnchance_police_gasmask});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleFirefighter", chance= Susceptible_spawnchance_firefighter});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleSurvivorBasic", chance= Susceptible_spawnchance_survivor_basic});
-table.insert(ZombiesZoneDefinition.Default,{name = "SusceptibleSurvivorAdvanced", chance= Susceptible_spawnchance_survivor_advanced});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleHazmat",                  chance = spawnChance * 0.01});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleMilitaryNBC",             chance = spawnChance * 0.01});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleMilitaryGasmask",         chance = spawnChance * 0.02});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptiblePoliceGasmask",           chance = spawnChance * 0.03});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleFirefighter",             chance = spawnChance * 0.03});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleMilitaryHighcommand",     chance = spawnChance * 0.05});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleSurvivorAdvanced",        chance = spawnChance * 0.04});
+table.insert(ZombiesZoneDefinition.Default, {name = "SusceptibleSurvivorBasic",           chance = spawnChance * 0.06});
